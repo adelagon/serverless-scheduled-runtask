c26d2c0ced69b59259989d2700a50ec4d57e797e
Alvin Delagon <me@adelagon.com>
2023-04-26 10:33:41 +0000

    Add FargateScheduled task example

--- serverless_scheduled_runtask/serverless_scheduled_runtask_stack.py
+++ serverless_scheduled_runtask/serverless_scheduled_runtask_stack.py
@@ -3,9 +3,12 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_iam as iam,
+    aws_ecs as ecs,
     aws_events as events,
     aws_events_targets as targets,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
+    aws_applicationautoscaling as appscaling,
+    aws_ecs_patterns as ecs_patterns
 )
 from constructs import Construct
 
@@ -14,9 +17,14 @@ class ServerlessScheduledRuntaskStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, config: dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        ### ECS Cluster
+        cluster = ecs.Cluster(
+            self, "ECSCluster",
+        )
+
         ### create schedules & tasks
         for sched in config['schedules']:
-            ## Configure lambda-type tasks
+            ## Configure lambda type tasks
             if sched['task']['type'] == "lambda":
                 task_name = sched['task']['name']
                 task_handler = sched['task']['handler']
@@ -49,8 +57,17 @@ class ServerlessScheduledRuntaskStack(Stack):
                 )
 
                 lambda_func.add_to_role_policy(sns_policy)
-        
 
-
-        
-        
+            ## Configure fargate type tasks
+            if sched['task']['type'] == "fargate":
+                task_name = sched['task']['name']
+                task = ecs_patterns.ScheduledFargateTask(
+                    self, task_name,
+                    cluster=cluster,
+                    scheduled_fargate_task_image_options=ecs_patterns.ScheduledFargateTaskImageOptions(
+                        image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+                        memory_limit_mib=512
+                    ),
+                    schedule=appscaling.Schedule.expression(sched['task']['cron']),
+                    platform_version=ecs.FargatePlatformVersion.LATEST
+                )
